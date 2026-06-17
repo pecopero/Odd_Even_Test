74078533f94fe4e803cd5f790ae69b1eef1ab626
Ivan D Adi <ivan.adi@sirclo.com>
2026-06-17 17:14:35 +0700

    Add file.js

---
+++ file.js
@@ -0,0 +1,11 @@
+// Expected result : [[1, 3, 5, 7, 9], [2, 4, 6, 8, 10]]
+// Direction : Return nested array first is odd value and second is even array from the array number
+const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function result(number) {
+  const odd = number.filter(num => num % 2 !== 0);
+  const even = number.filter(num => num % 2 === 0);
+  return [odd, even];
+}
+
+console.log(result(number));
